c916be550c5ab89dc02da002f741f6128e43454f
Hariharan <sdhariharan7@gmail.com>
2025-12-30 18:08:08 +0530

    Linear Search

---
+++ LinnearSeacrh.java
@@ -0,0 +1,10 @@
+public class LinnearSeacrh {
+ public static void main(String[] args) {
+    int[] arr={1,2,3,4,5};
+    int target=3;
+    for(int i=0;i<arr.length;i++)
+    {
+        if(i==target){System.err.println(i);break;}
+    }
+ }   
+}
