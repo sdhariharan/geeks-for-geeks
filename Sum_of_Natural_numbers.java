5cbf1b27f5690818415f80d9e62ec7fea5e4909e
Hariharan <sdhariharan7@gmail.com>
2026-01-04 20:45:35 +0530

    Sum of natural numbers in array

---
+++ Sum_of_Natural_numbers.java
@@ -0,0 +1,12 @@
+public class Sum_of_Natural_numbers {
+    public static void main(String[] args) {
+        int n=10;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+        System.out.println("Sum of Natural Numbers in array:"+sum);
+    }
+    
+}
